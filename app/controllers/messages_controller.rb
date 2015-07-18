dd4af2a1ce6c538a1db0ede6e4b8da533309a172
suzukisan22 <getty2017@gmail.com>
2015-07-18 07:42:49 +0000

    add edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,26 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
   def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
   end
   
+  def edit
+  end
+  
   ##ここから追記
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はトップページへダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      #保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  
   def create 
     @message = Message.new(message_params)
     if @message.save
@@ -34,4 +49,9 @@ class MessagesController < ApplicationController
   ##ここまで
   
   
+
+
+def set_message
+  @message = Message.find(params[:id])
+end
 end
